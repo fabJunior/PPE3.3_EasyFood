67462d0e4ea7c892fe203bb02647dc5e8b61c9be
Fabien-Junior FRESSE <fjfresse@geiffel.fr>
2016-12-02 13:36:57 +0100

    First commit

---
+++ src/com/test/dao/UtilisateurDao.java
@@ -0,0 +1,11 @@
+package com.test.dao;
+
+import com.test.beans.Utilisateur;
+
+public interface UtilisateurDao {
+
+    void creer( Utilisateur utilisateur ) throws DAOException;
+
+    Utilisateur trouver( String email ) throws DAOException;
+
+}
