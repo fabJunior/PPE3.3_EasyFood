a4640d78f1c1e781294606931c727c800111e59f
alphaliaud <jl.liaud@hotmail.fr>
2016-12-09 14:34:04 +0100

    Classe métier Resto

---
+++ src/com/test/beans/Resto.java
@@ -0,0 +1,85 @@
+package com.test.beans;
+
+public class Resto {
+	private int idR;
+	private String nomR;
+	private int numAdrR;
+	private String rueAdrR;
+	private int cpR;
+	private String villeR;
+	/**
+	 * @return the idR
+	 */
+	public int getIdR() {
+		return idR;
+	}
+	/**
+	 * @param idR the idR to set
+	 */
+	public void setIdR(int idR) {
+		this.idR = idR;
+	}
+	/**
+	 * @return the nomR
+	 */
+	public String getNomR() {
+		return nomR;
+	}
+	/**
+	 * @param nomR the nomR to set
+	 */
+	public void setNomR(String nomR) {
+		this.nomR = nomR;
+	}
+	/**
+	 * @return the numAdrR
+	 */
+	public int getNumAdrR() {
+		return numAdrR;
+	}
+	/**
+	 * @param numAdrR the numAdrR to set
+	 */
+	public void setNumAdrR(int numAdrR) {
+		this.numAdrR = numAdrR;
+	}
+	/**
+	 * @return the rueAdrR
+	 */
+	public String getRueAdrR() {
+		return rueAdrR;
+	}
+	/**
+	 * @param rueAdrR the rueAdrR to set
+	 */
+	public void setRueAdrR(String rueAdrR) {
+		this.rueAdrR = rueAdrR;
+	}
+	/**
+	 * @return the cpR
+	 */
+	public int getCpR() {
+		return cpR;
+	}
+	/**
+	 * @param cpR the cpR to set
+	 */
+	public void setCpR(int cpR) {
+		this.cpR = cpR;
+	}
+	/**
+	 * @return the villeR
+	 */
+	public String getVilleR() {
+		return villeR;
+	}
+	/**
+	 * @param villeR the villeR to set
+	 */
+	public void setVilleR(String villeR) {
+		this.villeR = villeR;
+	}
+	
+	
+	
+}
